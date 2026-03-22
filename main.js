80458515df7fd435d1d8a52fce6c2d2b6ae0a466
filippozuppini <zuppini.filippo@gmail.com>
2026-03-22 16:30:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const biciclette = [
+
+    {
+        nome: 'graziella',
+        peso: 20,
+    },
+    {
+        nome: 'luisa',
+        peso: 40,
+    },
+    {
+        nome: 'NT20LX',
+        peso: 10,
+    },
+    {
+        nome: 'AA127',
+        peso: 100,
+    },
+    {
+        nome: 'Super238',
+        peso: 30,
+    }    
+
+]
+
+
